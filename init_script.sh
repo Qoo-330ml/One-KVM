a271e1fb15e0aff69abe3c3a2c6122208b13d511
Qoo-330ml <51946091+Qoo-330ml@users.noreply.github.com>
2024-10-05 20:18:59 +0800

    Create init_script.sh

---
+++ init_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# 确保脚本以root权限运行
+if [ "$(id -u)" != "0" ]; then
+    echo "该脚本需要root权限执行" 1>&2
+    exit 1
+fi
+
+# 步骤1: 使用dd命令创建一个4GB的文件
+dd if=/dev/zero of=/root/pikvm_msd.img bs=1M count=4096
+
+# 步骤2: 使用mkfs.ext4命令格式化文件为ext4文件系统
+mkfs.ext4 /root/pikvm_msd.img
+
+# 步骤3: 在/etc/fstab追加新的挂载点
+echo '/root/pikvm_msd.img /var/lib/kvmd/msd  ext4  nofail,nodev,nosuid,noexec,rw,errors=remount-ro,data=jou>
+
+# 步骤4: 挂载文件系统
+mount /root/pikvm_msd.img
+
+# 步骤5: 编辑/etc/kvmd/override.yaml文件
+sed -i '/msd:/,+2 s/type: disabled/type: otg/' /etc/kvmd/override.yaml
+
+# 步骤6: 重启kvm服务
+systemctl restart kvmd-otg kvmd
+
+echo "脚本执行完毕"
